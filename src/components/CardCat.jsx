c73bb7d59bef52782b024dbcf3c93e6ac2fafc07
Lisa Downie <Lisadownie5@gmail.com>
2024-05-24 16:52:13 +0100

    added more cards

--- src/components/CardCat.jsx
+++ src/components/CardCat.jsx
@@ -26,6 +26,26 @@ const CardCat = () => {
                         <span className='font-bold text-amber-500'>Experiences</span>
                     </div>
                 </div>
+                <div
+                    className='p-1 m-1 transform  rounded-xl h-40 w-60 sm:h-64 sm:w-64 bg-white shadow-xl transition duration-300 hover:scale-105'>
+                    <div className='flex h-full justify-center items-center'>
+                        <span className='font-bold text-gray-500'>Travel</span>
+                    </div>
+                </div>
+
+                <div
+                    className='transform  rounded-xl h-40 w-40 sm:h-64 sm:w-64 bg-white shadow-xl transition duration-300 hover:scale-105'>
+                    <div className='flex h-full justify-center items-center'>
+                        <span className='font-bold text-green-600'>Hobbies</span>
+                    </div>
+                </div>
+
+                <div
+                    className='transform rounded-xl h-40 w-40 sm:h-64 sm:w-64 bg-white shadow-xl transition duration-300 hover:scale-105'>
+                    <div className='flex h-full justify-center items-center'>
+                        <span className='font-bold text-amber-500'>Experiences</span>
+                    </div>
+                </div>
             </div>
         </div>
     )
