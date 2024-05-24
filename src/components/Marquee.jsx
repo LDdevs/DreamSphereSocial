7188b07e3d0866d1f6e27aeb0fbd3a94da2ed571
Lisa Downie <Lisadownie5@gmail.com>
2024-05-24 17:24:52 +0100

    once more

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -7,7 +7,7 @@ const Marquee = () => {
             <div className="w-full inline-flex flex-nowrap">
                 <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none">
                     <li>
-                        <img src="icon.png" alt="Facebook" />
+                        <img src="../assets/icon2.png" alt="Facebook" />
                     </li>
                     <li>
                         <img src="../icon2.png" alt="Disney" />
