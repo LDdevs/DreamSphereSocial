a11389af771026f31bac412d925108141c5895cd
Lisa Downie <Lisadownie5@gmail.com>
2024-05-24 17:16:53 +0100

    add marquee

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Hero from './components/Hero'
 import Footer from './components/Footer'
 import FAQ from './components/FAQ'
 import CardCat from './components/CardCat'
+import Marquee from './components/Marquee'
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Marquee />
       <CardCat />
       <FAQ />
       <Footer />
---
+++ src/components/Marquee.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+// import Logo_white_alt from '../assets/logo-alt.png';
+
+const Marquee = () => {
+    return (
+        <div>
+            <div className="w-full inline-flex flex-nowrap">
+                <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none">
+                    <li>
+                        <img src="./Logo_whitealt.png" alt="Facebook" />
+                    </li>
+                    <li>
+                        <img src="./Logo_whitealt.png" alt="Disney" />
+                    </li>
+                    <li>
+                        <img src="./Logo_whitealt.png" alt="Airbnb" />
+                    </li>
+                    <li>
+                        <img src="./Logo_whitealt.png" alt="Apple" />
+                    </li>
+                    <li>
+                        <img src="./Logo_whitealt.png" alt="Spark" />
+                    </li>
+                    <li>
+                        <img src="./Logo_whitealt.png" alt="Samsung" />
+                    </li>
+                    <li>
+                        <img src="./Logo_whitealt.png" alt="Quora" />
+                    </li>
+                    <li>
+                        <img src="./Logo_whitealt.png" alt="Sass" />
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Marquee
