43eb29f0bc4b3be76e36ac4a830362b6232b49fa
Lisa Downie <Lisadownie5@gmail.com>
2024-05-24 14:38:53 +0100

    add title

--- src/components/CardCat.jsx
+++ src/components/CardCat.jsx
@@ -1,33 +1,34 @@
 // import React from 'react'
 
 const CardCat = () => {
-  return (
-    <div>
-        <h2>Categories</h2>
-        <div className="flex gap-8 flex-wrap justify-center bg-gray-300 h-screen py-10">
-    <div
-        className="transform  rounded-xl h-40 w-40 sm:h-64 sm:w-64 bg-white shadow-xl transition duration-300 hover:scale-105">
-        <div className="flex h-full justify-center items-center">
-            <span className="font-bold text-gray-500">Travel</span>
-        </div>
-    </div>
+    return (
+        <div>
 
-    <div
-        className="transform  rounded-xl h-40 w-40 sm:h-64 sm:w-64 bg-white shadow-xl transition duration-300 hover:rotate-180">
-        <div className="flex h-full justify-center items-center">
-            <span className="font-bold text-green-600">Hobbies</span>
-        </div>
-    </div>
+            <div className="flex gap-8 flex-wrap justify-center bg-gray-300 h-screen py-10">
+                <h2>Categories</h2>
+                <div
+                    className="transform  rounded-xl h-40 w-40 sm:h-64 sm:w-64 bg-white shadow-xl transition duration-300 hover:scale-105">
+                    <div className="flex h-full justify-center items-center">
+                        <span className="font-bold text-gray-500">Travel</span>
+                    </div>
+                </div>
 
-    <div
-        className="transform rounded-xl h-40 w-40 sm:h-64 sm:w-64 bg-white shadow-xl transition duration-300 hover:translate-x-4">
-        <div className="flex h-full justify-center items-center">
-            <span className="font-bold text-amber-500">Experiences</span>
+                <div
+                    className="transform  rounded-xl h-40 w-40 sm:h-64 sm:w-64 bg-white shadow-xl transition duration-300 hover:rotate-180">
+                    <div className="flex h-full justify-center items-center">
+                        <span className="font-bold text-green-600">Hobbies</span>
+                    </div>
+                </div>
+
+                <div
+                    className="transform rounded-xl h-40 w-40 sm:h-64 sm:w-64 bg-white shadow-xl transition duration-300 hover:translate-x-4">
+                    <div className="flex h-full justify-center items-center">
+                        <span className="font-bold text-amber-500">Experiences</span>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
-    </div>
-  )
+    )
 }
 
 export default CardCat
