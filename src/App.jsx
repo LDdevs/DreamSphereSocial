582fd42af4af2633cccd2658c689a5b34cc1eab7
Lisa Downie <Lisadownie5@gmail.com>
2024-05-24 14:37:35 +0100

    adding cards

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Footer from './components/Footer'
 import FAQ from './components/FAQ'
+import CardCat from './components/CardCat'
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <CardCat />
       <FAQ />
       <Footer />
       
