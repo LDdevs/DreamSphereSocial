7b20aa5a98f37abdd24801da06696e477c188202
Lisa Downie <Lisadownie5@gmail.com>
2024-05-24 19:40:10 +0100

    hide

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -31,7 +31,7 @@ const Marquee = () => {
             <img src="./sass.svg" alt="Sass" />
         </li>
     </ul>
-    <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll" aria-hidden="true">
+    <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll" aria-hidden="true overflow:hidden">
         <li>
             <img src="./facebook.svg" alt="Facebook" />
         </li>
