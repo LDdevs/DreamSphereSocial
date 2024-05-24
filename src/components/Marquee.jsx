9fcb03281deb212189041187de54a569046bafdb
Lisa Downie <Lisadownie5@gmail.com>
2024-05-24 17:18:59 +0100

    broken image fix

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -7,28 +7,28 @@ const Marquee = () => {
             <div className="w-full inline-flex flex-nowrap">
                 <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none">
                     <li>
-                        <img src="./Logo_whitealt.png" alt="Facebook" />
+                        <img src="../Logo_whitealt.png" alt="Facebook" />
                     </li>
                     <li>
-                        <img src="./Logo_whitealt.png" alt="Disney" />
+                        <img src="../Logo_whitealt.png" alt="Disney" />
                     </li>
                     <li>
-                        <img src="./Logo_whitealt.png" alt="Airbnb" />
+                        <img src="../Logo_whitealt.png" alt="Airbnb" />
                     </li>
                     <li>
-                        <img src="./Logo_whitealt.png" alt="Apple" />
+                        <img src="../Logo_whitealt.png" alt="Apple" />
                     </li>
                     <li>
-                        <img src="./Logo_whitealt.png" alt="Spark" />
+                        <img src="../Logo_whitealt.png" alt="Spark" />
                     </li>
                     <li>
-                        <img src="./Logo_whitealt.png" alt="Samsung" />
+                        <img src="../Logo_whitealt.png" alt="Samsung" />
                     </li>
                     <li>
-                        <img src="./Logo_whitealt.png" alt="Quora" />
+                        <img src="../Logo_whitealt.png" alt="Quora" />
                     </li>
                     <li>
-                        <img src="./Logo_whitealt.png" alt="Sass" />
+                        <img src="../Logo_whitealt.png" alt="Sass" />
                     </li>
                 </ul>
             </div>
