4b137a17c9120051559619403b67bc9f9c15e8ed
Lisa Downie <Lisadownie5@gmail.com>
2024-05-24 17:09:18 +0100

    sizing

--- src/components/CardCat.jsx
+++ src/components/CardCat.jsx
@@ -7,7 +7,7 @@ const CardCat = () => {
             <div className='flex gap-8 flex-wrap justify-center bg-gray-300 h-screen py-10'>
                 
                 <div
-                    className='p-1 m-1 transform  rounded-xl h-40 w-60 sm:h-64 sm:w-64 bg-white shadow-xl transition duration-300 hover:scale-105'>
+                    className='p-1 m-1 transform  rounded-xl h-40 w-40 sm:h-64 sm:w-64 bg-white shadow-xl transition duration-300 hover:scale-105'>
                     <div className='flex h-full justify-center items-center'>
                         <span className='font-bold text-gray-500'>Travel</span>
                     </div>
@@ -21,7 +21,7 @@ const CardCat = () => {
                 </div>
 
                 <div
-                    className='p-1 m-1 transform rounded-xl h-40 w-40 sm:h-64 sm:w-64 bg-white shadow-xl transition duration-300 hover:scale-105'>
+                    className='p-1 m-1 transform  rounded-xl h-40 w-40 sm:h-64 sm:w-64 bg-white shadow-xl transition duration-300 hover:scale-105'>
                     <div className='flex h-full justify-center items-center'>
                         <span className='font-bold text-amber-500'>Experiences</span>
                     </div>
