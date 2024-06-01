f1ec6b5a784f947a53339b6963b23a0fd2401c3c
Lisa Downie <Lisadownie5@gmail.com>
2024-06-01 13:29:55 +0100

    change bg this time

--- src/components/CardCat.jsx
+++ src/components/CardCat.jsx
@@ -1,14 +1,16 @@
 // import React from 'react'
+import Boat from '../assets/boat.png'
+
 
 const CardCat = () => {
     return (
         <div className='text-green-300 bg-[rgb(2, 0, 36)])]'>
             <h2 className='flex flex-wrap justify-center text-4xl py-1 bg-[rgb(2, 0, 36)])]'>Categories</h2>
-            <div className='flex gap-8 flex-wrap justify-center bg-gray-300 h-full py-10 bg-[rgb(2, 0, 36)])]'>
+            <div className='flex gap-8 flex-wrap justify-center  h-full py-10 bg-[rgb(2, 0, 36)])]'>
                 
                 <div
-                    className='bg-[rgb(2, 0, 36)])] p-1 m-1 transform  rounded-xl h-40 w-60 sm:h-64 sm:w-64 bg-white shadow-xl transition duration-300 hover:scale-105'>
-                    <div className='flex h-full justify-center items-center'>
+                    className=' p-1 m-1 transform  rounded-xl h-40 w-60 sm:h-64 sm:w-64 bg-white shadow-xl transition duration-300 hover:scale-105' >
+                    <div className='flex h-full justify-center items-center bg-[{}]' >
                         <span className='font-bold text-gray-500'>Travel</span>
                     </div>
                 </div>
