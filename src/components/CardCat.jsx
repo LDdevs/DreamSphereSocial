5d4adb308cc28546d245a6c6f5493c3f52192793
Lisa Downie <Lisadownie5@gmail.com>
2024-06-11 18:59:15 +0100

    try this one with text

--- src/components/CardCat.jsx
+++ src/components/CardCat.jsx
@@ -13,10 +13,8 @@ const CardCat = () => {
                 <div
                     id="travelcard"
                     className=' p-1 m-10 transform  rounded-xl h-40 w-60 sm:h-64 sm:w-64  shadow-xl transition duration-300 hover:scale-105' >
-                    <img className='rounded-full max-w-[300px] hover:scale-120 hover:rotate-6' src={me} alt="" />
-                    {/* <div  className='flex h-full justify-center items-center ' >
-                        <span className='font-bold text-gray-500'>Travel</span>
-                    </div> */}
+                    <img className='rounded-full max-w-[300px] hover:scale-120 hover:rotate-6' src={me} alt="Travel" />
+                    <div className="centered">Centered</div>
                 </div>
 
                 <div
