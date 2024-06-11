a3133fea3cf9b2314192f95acdfb9c39948ef550
Lisa Downie <Lisadownie5@gmail.com>
2024-06-11 19:54:10 +0100

    moving bits

--- src/components/CardCat.jsx
+++ src/components/CardCat.jsx
@@ -13,9 +13,9 @@ import life from '../assets/lifecard.png';
 const CardCat = () => {
     return (
         <div className='text-green-300 bg-[#020024] my-[10px] border-solid'>
-            <h2 className='flex flex-wrap justify-center text-4xl py-1  bg-[#020024]'>Categories</h2>
+            
             <div  className='flex gap-8 flex-wrap justify-center  h-full py-10 bg-[#020024]'>
-
+                <h2 className='flex flex-wrap justify-center text-4xl py-1  bg-[#020024]'>Categories</h2>
                 <div
                     id="travelcard"
                     className='flex justify-center align-middle p-1 m-10 transform  rounded-xl h-40 w-60 sm:h-64 sm:w-64  shadow-xl transition duration-300 hover:scale-105' >
