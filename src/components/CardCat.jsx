a947c83c37aec3b929d7c740d1a393091da90c17
Lisa Downie <Lisadownie5@gmail.com>
2024-06-11 18:23:50 +0100

    move id

--- src/components/CardCat.jsx
+++ src/components/CardCat.jsx
@@ -9,9 +9,9 @@ const CardCat = () => {
             <div className='flex gap-8 flex-wrap justify-center  h-full py-10 bg-[#020024]'>
 
                 <div
-                    id="travelcard"
+                    
                     className=' p-1 m-1 transform  rounded-xl h-40 w-60 sm:h-64 sm:w-64  shadow-xl transition duration-300 hover:scale-105' >
-                    <div className='flex h-full justify-center items-center ' >
+                    <div id="travelcard" className='flex h-full justify-center items-center ' >
                         <span className='font-bold text-gray-500'>Travel</span>
                     </div>
                 </div>
