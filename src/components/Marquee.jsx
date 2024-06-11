8a6024dd7226d7979dbdaa189c76f4234f8bfa64
Lisa Downie <Lisadownie5@gmail.com>
2024-06-11 20:27:45 +0100

    added items

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -12,9 +12,19 @@ const Marquee = () => {
                     <span className="mx-4 text-4xl">Marquee Item 3</span>
                     <span className="mx-4 text-4xl">Marquee Item 4</span>
                     <span className="mx-4 text-4xl">Marquee Item 5</span>
+                    <span className="mx-4 text-4xl">Marquee Item 1</span>
+                    <span className="mx-4 text-4xl">Marquee Item 2</span>
+                    <span className="mx-4 text-4xl">Marquee Item 3</span>
+                    <span className="mx-4 text-4xl">Marquee Item 4</span>
+                    <span className="mx-4 text-4xl">Marquee Item 5</span>
                 </div>
 
                 <div className="absolute top-0 py-12 animate-marquee2 whitespace-nowrap">
+                    <span className="mx-4 text-4xl">Marquee Item 1</span>
+                    <span className="mx-4 text-4xl">Marquee Item 2</span>
+                    <span className="mx-4 text-4xl">Marquee Item 3</span>
+                    <span className="mx-4 text-4xl">Marquee Item 4</span>
+                    <span className="mx-4 text-4xl">Marquee Item 5</span>
                     <span className="mx-4 text-4xl">Marquee Item 1</span>
                     <span className="mx-4 text-4xl">Marquee Item 2</span>
                     <span className="mx-4 text-4xl">Marquee Item 3</span>
