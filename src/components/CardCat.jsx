e3381f4c811b8083bb873795738c866eb04a0ad3
Lisa Downie <Lisadownie5@gmail.com>
2024-06-11 18:25:11 +0100

    move div up

--- src/components/CardCat.jsx
+++ src/components/CardCat.jsx
@@ -6,12 +6,12 @@ const CardCat = () => {
     return (
         <div className='text-green-300 bg-[#020024] my-[10px] border-solid'>
             <h2 className='flex flex-wrap justify-center text-4xl py-1  bg-[#020024]'>Categories</h2>
-            <div className='flex gap-8 flex-wrap justify-center  h-full py-10 bg-[#020024]'>
+            <div id="travelcard" className='flex gap-8 flex-wrap justify-center  h-full py-10 bg-[#020024]'>
 
                 <div
                     
                     className=' p-1 m-1 transform  rounded-xl h-40 w-60 sm:h-64 sm:w-64  shadow-xl transition duration-300 hover:scale-105' >
-                    <div id="travelcard" className='flex h-full justify-center items-center ' >
+                    <div  className='flex h-full justify-center items-center ' >
                         <span className='font-bold text-gray-500'>Travel</span>
                     </div>
                 </div>
