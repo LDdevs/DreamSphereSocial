c7a45ea0329cadd534a22bf54d19259ee07e9d3d
Lisa Downie <Lisadownie5@gmail.com>
2024-06-11 21:09:38 +0100

    moved

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -80,23 +80,23 @@ const FAQ = () => {
                             </div>
                         </div>
 
-                        <div>
-                            <div className="py-[auto] relative flex flex-nowrap overflow-x-hidden ">
-                                <div className="py-[auto] animate-marquee whitespace-nowrap">
-                                    <span className="mx-4 text-4xl">Login/Signup</span>
-                                    <span className="mx-4 text-4xl">Add Bucket List Items</span>
-                                    <span className="mx-4 text-4xl">Explore Others' lists</span>
-                                    <span className="mx-4 text-4xl">Share Experiences</span>
-                                    <span className="mx-4 text-4xl">Complete your bucket List IRL</span>
-                                </div>
-                            </div>
-                        </div>
+
 
                     </div>
                 </div>
             </div>
 
-
+            <div>
+                <div className="py-[auto] relative flex flex-nowrap overflow-x-hidden ">
+                    <div className="py-[auto] animate-marquee whitespace-nowrap">
+                        <span className="mx-4 text-4xl">Login/Signup</span>
+                        <span className="mx-4 text-4xl">Add Bucket List Items</span>
+                        <span className="mx-4 text-4xl">Explore Others lists</span>
+                        <span className="mx-4 text-4xl">Share Experiences</span>
+                        <span className="mx-4 text-4xl">Complete your bucket List IRL</span>
+                    </div>
+                </div>
+            </div>
 
         </div >
     )
