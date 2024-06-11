468b9a838908c0c57dde32d99c73f6593321b9aa
Lisa Downie <Lisadownie5@gmail.com>
2024-06-11 16:49:21 +0100

    remove bg color

--- src/components/CardCat.jsx
+++ src/components/CardCat.jsx
@@ -10,7 +10,7 @@ const CardCat = () => {
                 
                 <div
                 id="travelcard"
-                    className=' p-1 m-1 transform  rounded-xl h-40 w-60 sm:h-64 sm:w-64 bg-white shadow-xl transition duration-300 hover:scale-105' >
+                    className=' p-1 m-1 transform  rounded-xl h-40 w-60 sm:h-64 sm:w-64  shadow-xl transition duration-300 hover:scale-105' >
                     <div className='flex h-full justify-center items-center bg-[{}]' >
                         <span className='font-bold text-gray-500'>Travel</span>
                     </div>
