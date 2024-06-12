d19f26f7c30ac1e9bbb6d9bfb409d5aba453f634
Lisa Downie <Lisadownie5@gmail.com>
2024-06-12 21:49:37 +0100

    hfgh

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -54,7 +54,7 @@ const Navbar = () => {
 
                 {/* Hamburger */}
                 
-                <div onClick={() => setShowMenu (showMenu => !showMenu)} className='mx-[auto] xl:hidden flex justify-end p-[20px] bg-[#a5a9ae] hover:text-[rgb(2,0,36);] z-10 rounded-full'>
+                <div onClick={() => setShowMenu (showMenu => !showMenu)} className='mx-[auto] xl:hidden flex flex-wrap justify-end p-[20px] bg-[#a5a9ae] hover:text-[rgb(2,0,36);] z-10 rounded-full'>
                     {!nav ? <FaBars /> : <FaTimes />}
                 </div>
                     {showMenu && (
