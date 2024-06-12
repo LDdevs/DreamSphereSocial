6032f75fee63922ee95799737c52cfa9492dd556
Lisa Downie <Lisadownie5@gmail.com>
2024-06-12 22:14:45 +0100

    yghj

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -26,7 +26,7 @@ const Contact = () => {
 
 
                     <div id="hero-header" className="my-[50%]">
-                        <img className='min-w-[280] md:w-[380px] lg:w-[480px] mx-[auto] flex sm:w-auto h-[auto] justify-center align-middle' src={Logo} alt="" />
+                        <img className='min-w-[280px] md:w-[380px] lg:w-[480px] mx-[auto] flex sm:w-auto h-[auto] justify-center align-middle' src={Logo} alt="" />
                         <h1 id="title" className='text-white md:shrink-0 px-6 mx-[auto] flex text-4xl sm:text-7xl font-bold'>Contact Us</h1>
                         {/* <p className='text-white px-2 md:shrink-0 mx-[auto]'>Contact us below! </p> */}
 
