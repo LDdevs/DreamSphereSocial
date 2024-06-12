996bd9403e5a7373f0ebeed42395de3790fc752a
Lisa Downie <Lisadownie5@gmail.com>
2024-06-12 20:18:00 +0100

    p auto

--- src/routes/Featured.jsx
+++ src/routes/Featured.jsx
@@ -45,9 +45,9 @@ const Featured = () => {
                         <div className="h-48 w-full bg-gray-200 flex flex-col justify-between p-4 bg-cover bg-center">
                             <p>Bucket Item 1</p>
                             <div className="w-8 h-9 bg-white rounded flex items-center justify-center text-red-500">
-                                <button className='w-8 h-9 p-4 m-4 bg-white rounded flex items-center justify-center text-red-500'>Add to bucket list</button>
-                                <button className='w-8 h-9 p-4 m-4 bg-white rounded flex items-center justify-center text-red-500'>Heart</button>
-                                <button className='w-8 h-9 p-4 m-4 bg-white rounded flex items-center justify-center text-red-500'>See discussion</button>
+                                <button className='w-8 h-9 p-[auto] m-4 bg-white rounded flex items-center justify-center text-red-500'>Add to bucket list</button>
+                                <button className='w-8 h-9 p-[auto] m-4 bg-white rounded flex items-center justify-center text-red-500'>Heart</button>
+                                <button className='w-8 h-9 p-[auto] m-4 bg-white rounded flex items-center justify-center text-red-500'>See discussion</button>
                             </div>
                         </div>
 
