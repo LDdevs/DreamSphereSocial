84838e3563d77ea0a61264c6ab021fa18c968bff
Lisa Downie <Lisadownie5@gmail.com>
2024-06-12 19:00:02 +0100

    border color

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -33,7 +33,7 @@ const Contact = () => {
                         <div name='contact' className='w-[auto] h-[auto] bg-[#0a192f] flex justify-center items-center p-4'>
                             <form method='POST' action="https://getform.io/f/868c2a09-1ae3-4ca2-b376-8e6fae0f74ac" className='flex flex-col max-w-[600px] w-full'>
                                 <div className='pb-8'>
-                                    <p className='text-4xl font-bold inline border-b-4 border-[#ff66c4] text-gray-300'>Contact</p>
+                                    <p className='text-4xl font-bold inline border-b-4 border-[rgb(134,239,172);] text-gray-300'>Contact</p>
                                     <p className='text-gray-300 py-4 '>Submit the form below!</p>
                                 </div>
                                 <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
