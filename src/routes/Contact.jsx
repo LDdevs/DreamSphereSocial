16d2ad142d33879006ce56a0dc5ae98e427b9e2f
Lisa Downie <Lisadownie5@gmail.com>
2024-06-12 18:46:30 +0100

    taken awak email address

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -36,7 +36,7 @@ const Contact = () => {
         <form method='POST' action="https://getform.io/f/868c2a09-1ae3-4ca2-b376-8e6fae0f74ac" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-[#ff66c4] text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4 '>Submit the form below or send me an email - lisadownie5@gmail.com</p>
+                <p className='text-gray-300 py-4 '>Submit the form below</p>
             </div>
             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name'/>
             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email'/>
