5e552eab943b0a22ff43f275cb186c115bfecf57
Lisa Downie <Lisadownie5@gmail.com>
2024-06-12 22:10:17 +0100

    yggfg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -63,7 +63,7 @@ const Navbar = () => {
                 </div>
 
                 {/* Mobile Menu */}
-                <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center cursor-pointer'}>
+                <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] text-white flex flex-col justify-center items-center cursor-pointer'}>
                     <li className='py-6 text-4xl'><li><Link onClick={handleClick} to="/" smooth={true} duration={500} >
                         Home
                     </Link></li></li>
