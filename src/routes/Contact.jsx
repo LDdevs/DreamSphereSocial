f5088603c1330849b643be2dad03d6b5997e9d6d
Lisa Downie <Lisadownie5@gmail.com>
2024-06-12 22:16:10 +0100

    kljkl

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -22,11 +22,11 @@ const Contact = () => {
             <Navbar />
 
             <div id='hero' className=" h-full w-full z-1 flex justify-center  bg-full items-center text-center px-[80px] py-[7px] bg-cover bg-[url('/assets/boat.png')] text-[rgb(2,0,36);] bg-fixed">
-                <div className='h-full '>
+                <div className='h-full w-full'>
 
 
                     <div id="hero-header" className="my-[50%]">
-                        <img className='min-w-[280px] md:w-[380px] lg:w-[480px] mx-[auto] flex sm:w-auto h-[auto] justify-center align-middle' src={Logo} alt="" />
+                        <img className='min-w-[auto] md:w-[380px] lg:w-[480px] mx-[auto] flex sm:w-auto h-[auto] justify-center align-middle' src={Logo} alt="" />
                         <h1 id="title" className='text-white md:shrink-0 px-6 mx-[auto] flex text-4xl sm:text-7xl font-bold'>Contact Us</h1>
                         {/* <p className='text-white px-2 md:shrink-0 mx-[auto]'>Contact us below! </p> */}
 
