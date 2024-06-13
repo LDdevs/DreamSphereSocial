4919e50522d9cb94d22bb557389b92f47e3280d8
Lisa Downie <Lisadownie5@gmail.com>
2024-06-13 14:42:59 +0100

    uhjkh

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
         <div>
             <Navbar />
 
-            <div id='hero' className=" h-full w-full z-1 flex justify-center  bg-full items-center text-center px-[80px] py-[7px] bg-cover bg-[url('/assets/boat.png')] text-[rgb(2,0,36);] bg-fixed">
+            <div id='hero' className=" h-full w-full z-1 flex justify-center  bg-full items-center text-center px-[80px] py-[7px] bg-cover bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-[rgb(2,0,36);] bg-fixed">
                 <div className='h-[auto] w-full'>
 
 
