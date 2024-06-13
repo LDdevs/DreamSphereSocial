ac97fb41e1f8b30a81bb926a84961de6d0c4a769
Lisa Downie <Lisadownie5@gmail.com>
2024-06-13 15:05:32 +0100

    jkhjk

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -23,16 +23,10 @@ const Dashboard = () => {
         <div>
             <Navbar />
 
-<div id='hero' className="mx-[auto] w-full z-1 flex justify-center  bg-full items-center text-center px-[80px] py-[7px] bg-cover bg-[url('/assets/boat.png')] text-[rgb(2,0,36);] bg-fixed">
+<div id='hero' className="mx-[auto] w-full z-1 flex justify-center  bg-full items-center text-center px-[80px] py-[7px] bg-cover bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-[rgb(2,0,36);] bg-fixed">
 <div className='h-[100%] '>
 
     
-    <div id="hero-header" className="my-[50%]">
-        <img className='min-w-[280] md:w-[380px] lg:w-[480px] mx-[auto] flex sm:w-auto h-[auto] justify-center align-middle' src={Logo} alt="" />
-        <h1 id="title" className='text-white md:shrink-0 px-6 mx-[auto] flex text-4xl sm:text-7xl font-bold'>My Dashboard</h1>
-        <p className='text-white px-2 md:shrink-0 mx-[auto]'>Add to your own Bucket List </p>
-        
-    </div>
 </div>
 
 {/* CARDS */}
