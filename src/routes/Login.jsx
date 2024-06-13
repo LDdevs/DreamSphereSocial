6700a766f41e5180a210b5297792f17b7ded4fc2
Lisa Downie <Lisadownie5@gmail.com>
2024-06-13 14:27:48 +0100

    uihuuuiuji

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
                     <div id="hero-header" className='my-[auto]'>
                         <img className='min-w-[280] md:w-[380px] lg:w-[480px] mx-[auto] flex sm:w-auto h-[auto] justify-center align-middle' src={Logo} alt="" />
 
-                        <h1 id="title" className='text-white md:shrink-0 px-6 mx-[auto] flex flex-row text-4xl sm:text-7xl font-bold py-[7px] grid grid-cols-2 gap-4'>Login</h1>
+                        <h1 id="title" className='text-white md:shrink-0 px-6 mx-[auto]  text-4xl sm:text-7xl font-bold py-[7px] grid grid-cols-2 gap-4'>Login</h1>
                         <div className='bg-[#ccd6f67e] w-[auto] h-[auto] shadow-md rounded py-[7px]'>
                             <p className='text-[rgb(134,239,172);] px-2 md:shrink-0 mx-[auto]'>Log in below! </p>
                             <form action="#" className='flex flex-col max-w-[1000px] w-[auto]'>
@@ -41,7 +41,7 @@ const Login = () => {
 
 
 
-                        <h1 id="title" className=' text-white md:shrink-0 px-6 mx-[auto] flex flex-row text-4xl sm:text-7xl font-bold py-[7px] grid grid-cols-2 gap-4'>Sign Up</h1>
+                        <h1 id="title" className=' text-white md:shrink-0 px-6 mx-[auto]  text-4xl sm:text-7xl font-bold py-[7px] grid grid-cols-2 gap-4'>Sign Up</h1>
                         <div className=' bg-[#ccd6f67e] w-[auto] h-[auto] shadow-md rounded py-[7px]'>
                             <p className='text-[rgb(134,239,172);] px-2 py-[7px] md:shrink-0 mx-[auto]'>Log in below! </p>
                             <form action="#" className='flex flex-col max-w-[1000px] w-[auto]'>
