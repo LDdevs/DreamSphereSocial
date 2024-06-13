b8b12f07eb142b88185a0433ecd853606db3cd33
Lisa Downie <Lisadownie5@gmail.com>
2024-06-13 16:37:44 +0100

    fgghghf

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -47,9 +47,9 @@ const Dashboard = () => {
                 <div className='bg-slate-300  w-[280px] flex mx-[60px] my-[20px] p-[40px] text-white'>
                     <h1>Edit Dashboard</h1>
                 </div>
-                <div className='bg-slate-300 h-[800px] w-[800px] flex flex-wrap my-[60px] mx-[20px] justify-center px-[240px] py-[200px] text-white'>
+                {/* <div className='bg-slate-300 h-[800px] w-[800px] flex flex-wrap my-[60px] mx-[20px] justify-center px-[240px] py-[200px] text-white'>
                     <h1>Friends</h1>
-                </div>
+                </div> */}
             </div>
 
 
