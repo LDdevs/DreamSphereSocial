ecc9ec2468d2f37d6c9ef10fcad2fd938b4022e0
Lisa Downie <Lisadownie5@gmail.com>
2024-06-13 15:47:51 +0100

    ughhjghjg

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -34,9 +34,9 @@ const Dashboard = () => {
                 </div>
                 <div className='bg-slate-600  w-[180px] flex flex-wrap m-[60px]'>
                     <img src={[Avatar]} alt="" className='rounded-[30px] flex flex-wrap justify-center' />
-                    <h3>Your Name</h3>
-                    <h4>Location</h4>
-                    <h5>Active Today</h5>
+                    <h3 className='flex flex-wrap p-2 m-2'>Your Name</h3>
+                    <h4 className='flex flex-wrap p-2 m-2'>Location</h4>
+                    <h5 className='flex flex-wrap p-2 m-2'>Active Today</h5>
                 </div>
 
 
